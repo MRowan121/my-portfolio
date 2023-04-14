0167e7d7d3080df4c62ff0238829d748ffe69596
Matt Rowan <matt.rowan121@gmail.com>
2023-04-14 09:32:17 -0400

    Add conten to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import React from "react";
+import {AiFillLinkedin, AiFillGithub} from'react-icons/ai'
 
 const Header = () => {
     return (
         <header>
-
+            <h1>DEVELOPEDBYMATT</h1>
+            <ul>
+                <li>ABOUT</li>
+                <li>WORK</li>
+                <li>CONTACT</li>
+                <li>
+                    <AiFillLinkedin />
+                </li>
+                <li>
+                    <AiFillGithub />
+                </li>
+            </ul>
         </header>
     )
 }
