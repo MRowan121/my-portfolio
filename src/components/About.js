e391843bdb7fc7562b4ba44049867b11e410ec62
Matt Rowan <matt.rowan121@gmail.com>
2023-04-14 10:35:05 -0400

    Refactor fragment into section

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,18 @@
 import Image from "next/legacy/image";
-import React from "react";
 import headshot from '../../public/headshot.jpeg'
 
 const About = () => {
     return (
-        <>
-            <Image src={headshot} alt="my-headshot" layout="responsive" />
-            <p className='text-md py-5 leading-8 text-gray-800'>
+        <section className="grid grid-cols-2">
+            <Image 
+                src={headshot} 
+                alt="my-headshot" 
+                layout="responsive" 
+            />
+            <p className='text-md p-5 leading-8 text-gray-800 place-self-center'>
             I am an experienced software developer with a focus on front-end web development, boasting over 1,500 hours of hands-on coding experience. My expertise lies in crafting high-quality, responsive user interfaces, utilizing cutting-edge technologies and programming languages to create dynamic and intuitive web applications.
             </p>
-        </>
+        </section>
     )
 }
 
