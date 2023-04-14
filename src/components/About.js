a16966b09b95a008697ac9d49aa8b9cd3c479db4
Matt Rowan <matt.rowan121@gmail.com>
2023-04-14 10:22:04 -0400

    Add image component

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,11 @@
+import Image from "next/legacy/image";
 import React from "react";
+import headshot from '../../public/headshot.jpeg'
 
 const About = () => {
     return (
         <>
-            
+            <Image src={headshot} alt="my-headshot" layout="responsive" />
         </>
     )
 }
