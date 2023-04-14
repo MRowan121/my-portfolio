0f216d18b56567934a46fb608aefe54b0750306f
Matt Rowan <matt.rowan121@gmail.com>
2023-04-14 10:22:26 -0400

    Add about me text

--- src/components/About.js
+++ src/components/About.js
@@ -6,6 +6,9 @@ const About = () => {
     return (
         <>
             <Image src={headshot} alt="my-headshot" layout="responsive" />
+            <p className='text-md py-5 leading-8 text-gray-800'>
+            I am an experienced software developer with a focus on front-end web development, boasting over 1,500 hours of hands-on coding experience. My expertise lies in crafting high-quality, responsive user interfaces, utilizing cutting-edge technologies and programming languages to create dynamic and intuitive web applications.
+            </p>
         </>
     )
 }
